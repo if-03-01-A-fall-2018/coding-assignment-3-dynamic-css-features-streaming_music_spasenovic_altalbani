53094b7d01678c96d631d4b7d22d1686e80a1aa5
Spasenovic Bozidar <if160146@WS8A04.local>
2019-05-20 08:09:18 +0200

    Include Quiz to Menu-bar

--- STREAMING MUSIC/WEB-APP/audio.js
+++ STREAMING MUSIC/WEB-APP/audio.js
@@ -2,4 +2,4 @@ let audioSource = document.quuerySelector("audio");
 const PLAY_TIME = 4000
 
 audioSource.play();
-settimeout(()=>audioSource.paus(), PLAY_TIME)
+settimeout(()=>audioSource.paus(), PLAY_TIME);
